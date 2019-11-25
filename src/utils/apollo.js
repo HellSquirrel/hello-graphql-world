e4b4be679d65ebcc0bab47494195f07b157faeaf
HellSquirrel <zloymult@gmail.com>
2019-11-25 18:56:43 +0300

    Initial commit with apollo :)

---
+++ src/utils/apollo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { SchemaLink } from "apollo-link-schema";
+import { makeExecutableSchema } from "graphql-tools";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { graphql } from "graphql";
+
+const typeDefs = `
+  type Query {
+    items: [Int]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    items: () => [1, 2, 4]
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+graphql(
+  schema,
+  `
+    query {
+      items
+    }
+  `
+).then(result => console.log("Got result", result));
+
+console.log(schema);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new SchemaLink({ schema })
+});
+
+const Provider = ({ children }) => (
+  <ApolloProvider client={client}>{children}</ApolloProvider>
+);
+
+export default Provider;
